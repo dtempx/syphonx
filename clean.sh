4efbe594ad46d385caf8edf0905e2d0ba9e1dfc0
Dave Templin <dave.templin@pricespider.com>
2024-02-29 15:20:52 -0800

    fix packlist

--- clean.sh
+++ clean.sh
@@ -1,4 +1,3 @@
-rm -rf dist
 find . -name "*.js" ! -path "./node_modules/*" -type f -exec rm {} +
 find . -name "*.js.map" ! -path "./node_modules/*" -type f -exec rm {} +
 find . -name "*.d.ts" ! -path "./node_modules/*" -type f -exec rm {} +
